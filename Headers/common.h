3b347bc0bedcc39fb97c8e477e40e3a59a8f4ba3
Li ZhenHui <phetsi_w@etna-alternance.net>
2017-05-06 22:23:56 +0200

    Update common.h

--- Headers/common.h
+++ Headers/common.h
@@ -1,4 +1,4 @@
-#ifndef _PROJECT_COMMON_H_
-#define _PROJECT_COMMON_H_
+#ifndef _{PROJECT_NAME}_COMMON_H_
+#define _{PROJECT_NAME}_COMMON_H_
 
-#endif //_PROJECT_COMMON_H_
+#endif //_P{PROJECT_NAME}_COMMON_H_
